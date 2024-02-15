a85482881faba8383bd5a2e12026f7d4f74faa10
Son-EL <sonel.jtenorio23@gmail.com>
2024-02-15 12:05:32 +0300

    day 4

---
+++ Day 4/index.js
@@ -0,0 +1,62 @@
+ //------------1st Activity
+
+// var grade = 96;
+
+//  if (grade >= 90) {
+//     console.log("Grade: A");
+//  } else if (grade >= 80 && grade < 90) {
+//      console.log("Grade: B");
+//  } else if (grade >= 70 && grade < 80) {
+//      console.log("Grade: C");
+//  } else if (grade >= 60 && grade < 70) {
+//      console.log("Grade: D");
+//  } else {
+//      console.log("Grade: F");
+//  }
+
+
+// ------------2nd Acitvity
+
+
+ var start = 21;
+ var end = 25;
+
+ prompt 
+
+ for (var i = start; i <= end; i++) {
+     console.log(i);
+ }
+
+
+// -------------3rd Acitvity
+
+
+// var secretNumber = 25;
+// // // attempt tracking
+//  var attempts = 0;
+// // / guessedNumber
+//  var guessedNum;
+
+//  console.log("Welcome! Guess my Secret Number :)");
+
+// // //do this 1st then check in while
+//    do{
+//   guessedNum = Number(prompt("Enter a number:"));
+//          if (guessedNum < secretNumber){
+//          console.log("Too Low! Try again.");
+        
+//     }
+//      else if (guessedNum > secretNumber){
+//           console.log("Too High! Try again.");
+//      }
+//      else{
+//          console.log("Congratulations! You guessed it! :", secretNumber);
+//          console.log("It took this #:", attempts, "attempts");
+//      }
+
+//     attempts++;
+//  }while(guessedNum = secretNumber)
+
+
+
+
